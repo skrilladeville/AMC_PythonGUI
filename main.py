97ad8fd491bad61bbaedd167d1598ec827cdcd42
Nicolas Ogier <nicolas.ogier@gmail.com>
2020-10-22 23:06:22 +0200

    Gestion du nombre de point de chaque question.

--- main.py
+++ main.py
@@ -30,11 +30,21 @@ class Questionnaire:
             element = question.get('element', 'item')
             # Création d'un élément de question
             question_amc.append("\\element{"+element+"}{\n")
+            # On récupère le nombre de points, 1 par défaut
+            points = str(question.get('points', 1))
+            # On récupère la pénalité en cas d'erreur, 0 par défaut,
+            penalite = str(question.get('penalite', 0))
             # Si la question est simple, multiple
             if question.get('type') == 'simple' or question.get('type') == 'multiple':
                 question_type = 'question'
+                # e Réponse incohérente = 0 pt / v Aucune réponse = 0 pt / b  bonne réponse rapporte = points / m mauvaise reponse = penalite / Max Total de points de la question
+                insere_bareme = "\\bareme{e=0,v=0,b=" + \
+                    points+",m="+penalite+",MAX="+points+"}"
                 if question.get('type') == 'multiple':
                     question_type = 'questionmult'
+                    # e Réponse incohérente = 0 pt / v Aucune réponse = 0 pt / b  bonne réponse rapporte = points / m mauvaise reponse = points ÷ Nbre de propositions / Max Total de points de la question / p = plancher / d= élève par de 1 point
+                    insere_bareme = "\\bareme{e=0,v=0,d="+points + \
+                        ",p=0,b=0,m=-"+points+"/NB,MAX="+points+"}"
                 question_amc.append("\t\\begin{"+question_type+"}")
                 # Ajoute l'identifiant de la question
                 question_amc.append("{"+question['id']+"}\n")
