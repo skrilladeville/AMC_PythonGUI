13b6a6cb08e07d6ab191abffb9d4f06b4d30c6cb
Nicolas Ogier <nicolas.ogier@gmail.com>
2020-10-22 23:02:07 +0200

    Définition de la classe Questionnaire : 	- procédure d'import fichier JSON

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+
+
+class Questionnaire:
+
+    questionnaire = []  # Questionnaire en cours
+    fichier_json = ''  # Nom du fichier json contenant les questions
+
+    def __init__(self,  questions_listes=[], questions_fichier='myjson'):
+        self.questionnaire = questions_listes
+        self.fichier_json = questions_fichier
+
+    def importer_json(self):
+        """
+        Importe le fichier JSON contenant les questions dans la variable questionnaire.
+        """
+        with open(self.fichier_json, "r") as mj:
+            self.questionnaire = json.load(mj)
+
+
+if __name__ == "__main__":
+    AMC = Questionnaire()
+    AMC.importer_json()
