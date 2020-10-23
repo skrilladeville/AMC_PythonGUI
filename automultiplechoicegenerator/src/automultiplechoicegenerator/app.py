9b721d99ad029eaa4552bbf7795c94a37b8349fa
Nicolas Ogier <nicolas.ogier@gmail.com>
2020-10-23 18:08:25 +0200

    Ajout GUI minimale de test

--- automultiplechoicegenerator/src/automultiplechoicegenerator/app.py
+++ automultiplechoicegenerator/src/automultiplechoicegenerator/app.py
@@ -17,11 +17,30 @@ class AmcGenerator(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box()
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+
+        name_label = toga.Label(
+            'Fichier AMC: ',
+            style=Pack(padding=(0, 5))
+        )
+        self.name_input = toga.TextInput(style=Pack(flex=1))
+        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        name_box.add(name_label)
+        name_box.add(self.name_input)
+        buttonConvert = toga.Button(
+            'Convertir pour AMC!',
+            on_press=self.start_convert,
+            style=Pack(padding=5)
+        )
+        main_box.add(name_box)
+        main_box.add(button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+
+    def start_convert(self, widget):
+        print('Start conversion JSON to AMC Tex file')
 
 
 def main():
