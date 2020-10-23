451343c61b12295d515033ec0a143ab15cedec9c
Nicolas Ogier <nicolas.ogier@gmail.com>
2020-10-23 22:53:34 +0200

    Refactorisation : - changement de nom de variable d'éléments d'interface pour être plus explicite. - modification nom de méthode le lecture du fichier JSON (start_convert en lire_json)

--- automultiplechoicegenerator/src/automultiplechoicegenerator/app.py
+++ automultiplechoicegenerator/src/automultiplechoicegenerator/app.py
@@ -29,36 +29,37 @@ class AmcGenerator(toga.App):
         name_box.add(name_label)
         name_box.add(self.name_input)
         # Bouton pour convertir le JSON
-        buttonConvert = toga.Button(
+        buttonLireJson = toga.Button(
             'Lire fichier JSON',
-            on_press=self.start_convert,
+            on_press=self.lire_json,
             style=Pack(padding=5)
         )
         # Zone pour afficher le résultat.
 
-        self.texteZone = toga.MultilineTextInput(
+        self.texteZoneJson = toga.MultilineTextInput(
             id='view1', style=Pack(flex=1), readonly=True)
+        self.texteZoneJson.MIN_HEIGHT = 200
 
         text_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        text_box.add(self.texteZone)
+        text_box.add(self.texteZoneJson)
 
         main_box.add(name_box)
-        main_box.add(buttonConvert)
+        main_box.add(buttonLireJson)
         main_box.add(text_box)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
-    def start_convert(self, widget):
+    def lire_json(self, widget):
         self.main_window.info_dialog(
-            title="Convertion", message="Conversion du JSON en AMC")
+            title="Fichier JSON", message="Lecture du fichier JSON")
         print('Start conversion JSON to AMC Tex file')
         question_liste = []
         fichier_json = self.name_input.value
         AMC = Questionnaire(question_liste, fichier_json)
         AMC.importer_json()
-        self.texteZone.value = AMC.afficher_questionnaire()
+        self.texteZoneJson.value = AMC.afficher_questionnaire()
 
 
 def main():
